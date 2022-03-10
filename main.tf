dfdc0cc5594f48fde452dd25abbfc555f7f25668
jvallexm <jvallexm@gmail.com>
2022-03-10 13:15:28 -0500

    Added wrapper

--- main.tf
+++ main.tf
@@ -12,38 +12,20 @@ provider "ibm" {
 
 
 ##############################################################################
-# Resource Group where VPC Resources Will Be Created
+# VPC Module
 ##############################################################################
 
-data "ibm_resource_group" "resource_group" {
-  name = var.resource_group
-}
-
-##############################################################################
-
-
-##############################################################################
-# Create VPC
-##############################################################################
-
-module "vpc" {
-  source                      = "./vpc"
-  resource_group_id           = data.ibm_resource_group.resource_group.id
-  region                      = var.region
-  tags                        = var.tags
-  prefix                      = lookup(local.override, "prefix", var.prefix)
-  vpc_name                    = lookup(local.override, "vpc_name", local.config.vpc_name)
-  classic_access              = lookup(local.override, "classic_access", local.config.classic_access)
-  network_acls                = lookup(local.override, "network_acls", local.acls)
-  use_public_gateways         = lookup(local.override, "use_public_gateways", local.config.use_public_gateways)
-  subnets                     = lookup(local.override, "subnets", local.config.subnets)
-  use_manual_address_prefixes = lookup(local.override, "use_manual_address_prefixes", null)
-  default_network_acl_name    = lookup(local.override, "default_network_acl_name", null)
-  default_security_group_name = lookup(local.override, "default_security_group_name", null)
-  default_routing_table_name  = lookup(local.override, "default_routing_table_name", null)
-  address_prefixes            = lookup(local.override, "address_prefixes", null)
-  routes                      = lookup(local.override, "routes", [])
-  vpn_gateways                = lookup(local.override, "vpn_gateways", [])
+module "ez_vpc" {
+  source                = "./ez_vpc"
+  prefix                = var.prefix
+  region                = var.region
+  resource_group        = var.resource_group
+  tags                  = var.tags
+  use_public_gateways   = var.use_public_gateways
+  add_cluster_acl_rules = var.add_cluster_acl_rules
+  allow_inbound_traffic = var.allow_inbound_traffic
+  classic_access        = var.classic_access
+  override_json         = var.override_json
 }
 
 ##############################################################################
