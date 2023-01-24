2e44f8fe8d68cf27e978bc921eeded8ea24bffb9
jvallexm <jvallexm@gmail.com>
2023-01-24 10:00:25 -0500

    fix: no longer has json data that will force incorrect json parse

--- ez_vpc/outputs.tf
+++ ez_vpc/outputs.tf
@@ -1,10 +1,49 @@
 ##############################################################################
-# Output Configuration
+# VPC GUID
 ##############################################################################
 
-output "config" {
-  description = "Output configuration as encoded JSON"
-  value       = data.external.format_output.result.data
+output "vpc_id" {
+  description = "ID of VPC created"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_crn" {
+  description = "CRN of VPC created"
+  value       = module.vpc.vpc_crn
+}
+
+##############################################################################
+
+
+##############################################################################
+# VPN Gateway Public IPs
+##############################################################################
+
+output "vpn_gateway_public_ips" {
+  description = "List of VPN Gateway public IPS"
+  value       = module.vpc.vpn_gateway_public_ips
+}
+
+##############################################################################
+
+
+##############################################################################
+# Subnet Outputs
+##############################################################################
+
+output "subnet_ids" {
+  description = "The IDs of the subnets"
+  value       = module.vpc.subnet_ids
+}
+
+output "subnet_detail_list" {
+  description = "A list of subnets containing names, CIDR blocks, and zones."
+  value       = module.vpc.subnet_detail_list
+}
+
+output "subnet_zone_list" {
+  description = "A list containing subnet IDs and subnet zones"
+  value       = module.vpc.subnet_zone_list
 }
 
 ##############################################################################
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,49 @@
 ##############################################################################
-# Output Configuration
+# VPC GUID
 ##############################################################################
 
-output "config" {
-  description = "Output configuration as encoded JSON"
-  value       = module.ez_vpc.config
+output "vpc_id" {
+  description = "ID of VPC created"
+  value       = module.ez_vpc.vpc_id
+}
+
+output "vpc_crn" {
+  description = "CRN of VPC created"
+  value       = module.ez_vpc.vpc_crn
 }
 
 ##############################################################################
 
+
+##############################################################################
+# VPN Gateway Public IPs
+##############################################################################
+
+output "vpn_gateway_public_ips" {
+  description = "List of VPN Gateway public IPS"
+  value       = module.ez_vpc.vpn_gateway_public_ips
+}
+
+##############################################################################
+
+
+##############################################################################
+# Subnet Outputs
+##############################################################################
+
+output "subnet_ids" {
+  description = "The IDs of the subnets"
+  value       = module.ez_vpc.subnet_ids
+}
+
+output "subnet_detail_list" {
+  description = "A list of subnets containing names, CIDR blocks, and zones."
+  value       = module.ez_vpc.subnet_detail_list
+}
+
+output "subnet_zone_list" {
+  description = "A list containing subnet IDs and subnet zones"
+  value       = module.ez_vpc.subnet_zone_list
+}
+
+##############################################################################
